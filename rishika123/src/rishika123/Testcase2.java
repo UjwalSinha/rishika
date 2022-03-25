52d60c07f231ad07b993acedeaa7946e6b35db91
rgopagani <rgopagani@CUP-IOC-627.ad.cambridge.org>
2022-03-25 09:16:48 +0530

    First commit - Test case 2

--- rishika123/src/rishika123/Testcase2.java
+++ rishika123/src/rishika123/Testcase2.java
@@ -1,11 +1,11 @@
 package rishika123;
 
-public class Test1 {
+public class Testcase2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test case 1");
-		System.out.println("Testcase 2");
+		System.out.println("This is test case 2");
+
 	}
-  
+
 }
